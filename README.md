3631dec45f458de2c3348bb3a125fa450a8ba9f7
diyooor <diyorsattarov7@gmail.com>
2023-08-29 01:43:04 -0500

    refactor: Implement asynchronous file reading and error handling
    - Modify server.js to use async/await for reading log files
    - Improve error handling in log fetching
    - Use fs/promises for asynchronous file operations
    
    This change enhances the code's efficiency and reliability by adopting modern asynchronous practices and improving error handling in log fetching.

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # webapp-prac
 
+Welcome to webapp-prac, a simple web application that provides a log viewer for server logs. This application is built using Node.js and Express.js, with logging capabilities provided by the versatile logger middleware, Winston.
+
 ## Features
+
+- Provides a user-friendly web interface for viewing server logs.
+- Fetches and displays logs from log files: `combined.log` and `error.log`.
+- Logs are displayed in a scrollable console-like box.
 
 ## Installation
 
 1. Clone this repository to your local machine.
 2. Install Node.js and npm if not already installed.
 3. Navigate to the project directory and run the following command to install dependencies:
+
 ```
 npm install express ejs winston
 ```
+
 
 ## Dependencies
 
@@ -18,8 +26,24 @@ npm install express ejs winston
 
 ## Usage
 
+1. After installing dependencies, start the server by running:
+```
+node server.js
+```
+
+2. Open a web browser and navigate to `http://localhost:3000` to access the log viewer.
+
 ## Directory Structure
+
+- `views/`: Contains EJS templates for rendering the HTML pages.
+- `static/`: Contains static assets such as CSS files.
+- `server.js`: The main application file that configures the server and routes.
+- `logger.js`: Custom logger configuration using Winston.
 
 ## Contributing
 
+Contributions to this project are welcome. Feel free to open issues or submit pull requests.
+
 ## License
+
+This project is licensed under the MIT License.
--- server.js
+++ server.js
@@ -1,49 +1,72 @@
+// Import necessary modules and libraries
 const express = require('express');
 const path = require('path');
-const logger = require('./logger'); 
+const winston = require('winston');
+const fs = require('fs/promises');
 
+// Create a logger instance
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(info => {
+            return `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}`;
+        })
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' })
+    ]
+});
+
+// Create an Express app instance
 const app = express();
 const port = process.env.PORT || 3000;
-  
-logger.info('Server starting...');
 
+// Set up view engine and static files
 app.set('view engine', 'ejs');
-
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'static')));
 
+// Middleware to log requests
 app.use((req, res, next) => {
     logger.info(`Request made to: ${req.method} ${req.url}`);
     next();
 });
 
+// Endpoint to serve favicon
 app.get('/favicon.ico', (req, res) => res.status(204));
 
+// Endpoint to render the index page
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/logs', (req, res) => {
-    const logs = readLogsFromFiles();
-    res.json(logs);
-});
-
-function readLogsFromFiles() {
-    const fs = require('fs');
-    const logs = [];
-    
+// Endpoint to fetch logs
+app.get('/logs', async (req, res) => {
     try {
-        const combinedLog = fs.readFileSync('combined.log', 'utf8');
-        const errorLog = fs.readFileSync('error.log', 'utf8');
-        logs.push(...combinedLog.split('\n'), ...errorLog.split('\n'));
+        const logs = await readLogsFromFiles();
+        res.json(logs);
     } catch (error) {
         logger.error('Error reading log files:', error);
+        res.status(500).json({ error: 'An error occurred while fetching logs.' });
     }
+});
 
-    return logs.filter(log => log.trim() !== '');
+// Function to read logs from files
+async function readLogsFromFiles() {
+    try {
+        const combinedLog = await fs.readFile('combined.log', 'utf8');
+        const errorLog = await fs.readFile('error.log', 'utf8');
+        const logs = [...combinedLog.split('\n'), ...errorLog.split('\n')];
+        return logs.filter(log => log.trim() !== '');
+    } catch (error) {
+        throw error;
+    }
 }
 
+// Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    logger.info(`Server is running on port ${port}`);
 });
